06baa3c7d96dbd7387a034e5d122a4e339c8e2b0
Tannr Allard <rockinghamtannr@gmail.com>
2017-05-11 23:05:44 -0400

    Uploading first part of tictactoe

--- lesson_6/tic_tac_toe_walkthrough.rb
+++ lesson_6/tic_tac_toe_walkthrough.rb
@@ -1,15 +1,39 @@
 # Set up and display the board
+def prompt(msg)
+    puts "=> #{msg}"
+end 
+ 
+ def display_board(brd)
+    puts ""
+    puts "     |     |     "
+    puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
+    puts "     |     |     "
+    puts "-----+-----+-----"
+    puts "     |     |     "
+    puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}  "
+    puts "     |     |     "
+    puts "-----+-----+-----"
+    puts "     |     |     "
+    puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
+    puts "     |     |     "
+    puts ""
+end
 
- puts ""
- puts "		|	  | 	"
- puts "		|	  | 	"
- puts "		|	  | 	"
- puts "-----+-----+-----"
- puts "		|	  | 	"
- puts "		|	  | 	"
- puts "		|	  | 	"
- puts "-----+-----+-----"
- puts "		|	  | 	"
- puts "		|	  | 	"
- puts "		|	  | 	"
- puts ""
+def initialize_board
+    new_board = []
+    (1..9).each {|num| new_board[num] = ' '}
+    new_board
+end 
+
+def player_move(brd)
+    prompt "Choose a square (1-9):"
+    square = gets.chomp.to_i
+    brd[square] = 'X'
+end 
+
+board = initialize_board
+display_board(board)
+
+player_move(board)
+puts board.inspect
+display_board(board)
