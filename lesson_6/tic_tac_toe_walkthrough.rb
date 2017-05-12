fe145f491f13b5d771f6ab21ada63e6501cdf695
Tannr Allard <rockinghamtannr@gmail.com>
2017-05-12 17:31:11 -0400

    Updating tictactoe walkthrough

--- lesson_6/tic_tac_toe_walkthrough.rb
+++ lesson_6/tic_tac_toe_walkthrough.rb
@@ -1,4 +1,9 @@
 # Set up and display the board
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
 def prompt(msg)
     puts "=> #{msg}"
 end 
@@ -21,19 +26,41 @@ end
 
 def initialize_board
     new_board = []
-    (1..9).each {|num| new_board[num] = ' '}
+    (1..9).each {|num| new_board[num] = INITIAL_MARKER}
     new_board
 end 
 
-def player_move(brd)
-    prompt "Choose a square (1-9):"
-    square = gets.chomp.to_i
-    brd[square] = 'X'
+def empty_squares(brd)
+    #Iterate over brd 
+    #If brd[index] == ' ' put index into a new array as an integer
+    # Return array of integers that represent available indices
+    available_squares = []
+    number = 1
+    loop do 
+        break if number > 9
+        if brd[number] == INITIAL_MARKER
+            available_squares << number
+        end
+        number +=1
+    end
+    available_squares
+end
+
+def player_move!(brd)
+    square = ''
+    loop do
+        prompt "Choose a square (#{empty_squares(brd).join(',')}):"
+        square = gets.chomp.to_i
+        break if empty_squares(brd).include?(square)
+        prompt "Sorry, that's not a valid choice."
+    end
+
+    brd[square] = PLAYER_MARKER
 end 
 
 board = initialize_board
 display_board(board)
 
-player_move(board)
+player_move!(board)
 puts board.inspect
 display_board(board)
